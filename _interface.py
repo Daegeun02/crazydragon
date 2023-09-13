e125a01f41d088bcf8f6df4f9e6769f50291222d
Daegeun02 <redhawkdg02@gmail.com>
2023-09-13 16:05:11 +0900

    edit

--- _interface.py
+++ _interface.py
@@ -1,3 +1,6 @@
+import asyncio
+from asyncio import create_task
+
 from CrazyDG import CrazyDragon
 
 from threading import Thread
@@ -5,12 +8,27 @@ from threading import Thread
 from time import sleep
 
 
+async def print_state( _cf: CrazyDragon, t ):
+    print( "\033[A\033[K", _cf.pos, _cf.vel, _cf.command, t )
+
+async def _Timer( dt ):
+    await asyncio.sleep( dt )
+
+async def Print( _cf, t, dt ):
+
+    timer = create_task( _Timer( dt ) )
+    _task = create_task( print_state( _cf, t ) )
+
+    await timer
+    await _task
+
 
 def interface( _cf: CrazyDragon, thread: Thread, t ):
 
+    dt = 0.01
+
     while thread.is_alive():
-        print( "\033[A\033[K", _cf.pos, _cf.vel, _cf.command, t )
 
-        sleep( 0.01-0.002)
+        asyncio.run( Print( _cf, t, dt ) )
 
-        t += 0.01
+        t += dt
